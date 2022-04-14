54e8eab21fe61e5dade308d2ce8149370c9f6faa
Kalapoorani <88643861+Kalapoorani-P@users.noreply.github.com>
2022-04-14 13:51:18 +0530

    Update customeDataset.py

--- kp/DatasetAndDataloader/customeDataset.py
+++ kp/DatasetAndDataloader/customeDataset.py
@@ -13,8 +13,7 @@ class MyDataSet(Dataset) :
         self.allImagePath = []
         self.allLabels = []
         self.Imageclass = os.listdir(self.path)
-        # print(self.labelOfImage)
-        # print(self.labeImage)
+       
         for labelno,label in enumerate(self.Imageclass):
             for images in os.listdir(self.path+"\\"+label):
                 self.allImagePath.append(self.path + "\\" + label + "\\" + images)
@@ -22,7 +21,7 @@ class MyDataSet(Dataset) :
         self.transforms = lambda img: cv2.resize(img,(500,500))
         self.normalize = lambda imgNorm: 2*((imgNorm - np.amin(imgNorm ))/ (np.amax(imgNorm) - np.amin(imgNorm))) - 1
     def __getitem__(self,index) :
-        # print("GetItem function",index)
+       
         img = cv2.imread(self.allImagePath[index])
         label = self.allLabels[index]
         img = self.transforms(img)
@@ -35,15 +34,15 @@ class CustomeSampler(Sampler):
          self.dataset = dataset
          self.SequenceType = SequenceType.lower()
     def __iter__(self):
-        # print("Starting Sampler")
+       
         if self.SequenceType =='s':
             for i in range(len(self.dataset)):
                 yield i
         elif self.SequenceType =="r":
             shuffledIndexList = random.sample(range(len(self.dataset)),len(self.dataset))
-            # print(shuffledIndexList)
+           
             for i in shuffledIndexList :
-                # print("Sampler Function",i)
+               
                 yield i
         else:
             print("Invalid SequenceType")
@@ -59,22 +58,8 @@ class CustomeBatchSampler:
     def __iter__(self):
         print("CustomeBatchSampler")
         batch = []
-        # sampler=iter(self.sampler)
-        # while 1:
-        #     try:
-        #         batch.append(next(sampler))
-        #        
-        #         if len(batch)==self.batch_size:
-        #             yield batch 
-        #             batch = []
-                
-        #     except:
-        #         if len(batch) > 0 and not self.drop_last:
-        #             yield batch
-        #         break
+       
         for indx in self.sampler:
-            # print("calling sampler",count,indx)
-        
             batch.append(indx)
             # print("Batch",batch)
             if len(batch) == self.batch_size:
@@ -108,8 +93,7 @@ if __name__ == '__main__':
 
     myBatchSampler = CustomeBatchSampler(mySampler,batch_size=12)
 
-    dataloader = DataLoader(myData,batch_sampler=myBatchSampler, collate_fn=collate_fn, num_workers=1)
-    print(dataloader)
-    # print(next(iter(dataloader)))
+    dataloader = DataLoader(myData,batch_sampler=myBatchSampler, collate_fn=collate_fn, num_workers=2)
+    
     for (img,traget) in dataloader:
         print((img.shape,traget))
