ce010c80e2cf01afed9dc3a2f4fc1b543150c73d
Kalapoorani <88643861+Kalapoorani-P@users.noreply.github.com>
2022-04-14 13:53:25 +0530

    Delete tempCodeRunnerFile.py

--- kp/DatasetAndDataloader/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-import torch 
-from torch.utils.data import Dataset,DataLoader
-from torchvision import datasets,transforms
-import os
-
-transform = transforms.Compose([transforms.Resize([500,500])])
-path = "C:\\Users\\test\\Desktop\\kp\\DatasetAndDataloader\\Data"
-myData = datasets.ImageFolder(path,transforms=transform)
